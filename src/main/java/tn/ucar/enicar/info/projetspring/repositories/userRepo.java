d7412ef649b65f93a39c89615b65dc4102550d66
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-02-27 22:03:22 +0100

    Refactor entity class names and attributes to follow Java naming conventions

--- src/main/java/tn/ucar/enicar/info/projetspring/repositories/userRepo.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/repositories/userRepo.java
@@ -1,9 +1,9 @@
 package tn.ucar.enicar.info.projetspring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import tn.ucar.enicar.info.projetspring.entities.user;
+import tn.ucar.enicar.info.projetspring.entities.User;
 
-public interface userRepo extends JpaRepository<user, Integer> {
+public interface userRepo extends JpaRepository<User, Integer> {
 
 
 }
