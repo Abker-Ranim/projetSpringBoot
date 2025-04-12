fd813d27c3391687308384f870be221e6bbac7be
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-04-12 14:43:52 +0100

    refactor: adjust indentation in AuthenticationController for consistent formatting

--- src/main/java/tn/ucar/enicar/info/projetspring/auth/AuthenticationController.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/auth/AuthenticationController.java
@@ -1,32 +1,32 @@
-package tn.ucar.enicar.info.projetspring.auth;
+    package tn.ucar.enicar.info.projetspring.auth;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/v1/auth")
+    @RestController
+    @RequestMapping("/api/v1/auth")
 
-public class AuthenticationController {
-    private final AuthenticationService service;
+    public class AuthenticationController {
+        private final AuthenticationService service;
 
-    public AuthenticationController(AuthenticationService service) {
-        this.service = service;
+        public AuthenticationController(AuthenticationService service) {
+            this.service = service;
+        }
+        @PostMapping("/register")
+        public ResponseEntity<AuthenticationResponse>register(
+                @RequestBody RegisterRequest request
+        ){
+            return ResponseEntity.ok(service.register(request));
+        }
+
+        @PostMapping("/authenticate")
+        public ResponseEntity<AuthenticationResponse> authenticate(
+                @RequestBody AuthenticationRequest request
+        ) {
+            return ResponseEntity.ok(service.authenticate(request));
+        }
     }
-    @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse>register(
-            @RequestBody RegisterRequest request
-    ){
-        return ResponseEntity.ok(service.register(request));
-    }
-
-    @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
-            @RequestBody AuthenticationRequest request
-    ) {
-        return ResponseEntity.ok(service.authenticate(request));
-    }
-}
