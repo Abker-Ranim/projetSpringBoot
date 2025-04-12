7e2047097b783873f9fca3f8d639a599c57752a8
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-04-12 14:43:45 +0100

    refactor: remove unused imports in AdminService to improve code cleanliness

--- src/main/java/tn/ucar/enicar/info/projetspring/sevices/AdminService.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/sevices/AdminService.java
@@ -1,14 +1,8 @@
 package tn.ucar.enicar.info.projetspring.sevices;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import tn.ucar.enicar.info.projetspring.auth.AuthenticationResponse;
-import tn.ucar.enicar.info.projetspring.auth.AuthenticationService;
-import tn.ucar.enicar.info.projetspring.auth.RegisterRequest;
-import tn.ucar.enicar.info.projetspring.entities.Role;
-import tn.ucar.enicar.info.projetspring.entities.User;
-import tn.ucar.enicar.info.projetspring.repositories.userRepo;
+
 
 import java.util.UUID;
 
