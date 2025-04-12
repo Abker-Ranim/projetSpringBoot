e9752ce0e5c5a1be0e73b64722acf7e01f930ac7
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-04-12 14:45:20 +0100

    feat: add RoleRequestInputDTO for role request input data transfer

---
+++ src/main/java/tn/ucar/enicar/info/projetspring/auth/RoleRequestInputDTO.java
@@ -0,0 +1,10 @@
+package tn.ucar.enicar.info.projetspring.auth;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleRequestInputDTO {
+    private String requestedRole;
+}
