c1cdd12127894b9b0d5f7db677e5107a5cd5d05c
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-04-12 14:45:54 +0100

    feat: rename ManagementController to VoluntaryController for clarity

--- src/main/java/tn/ucar/enicar/info/projetspring/controllers/VoluntaryController.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/controllers/VoluntaryController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/management")
 
-public class ManagementController {
+public class VoluntaryController {
 
     @GetMapping
     public String get() {
