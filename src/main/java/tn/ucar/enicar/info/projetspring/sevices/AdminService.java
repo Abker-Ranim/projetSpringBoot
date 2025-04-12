0fe7e61f3564491de0902714c0ccc0db90163fc5
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-04-12 14:43:03 +0100

    refactor: remove createManager method from AdminService to streamline manager creation process

--- src/main/java/tn/ucar/enicar/info/projetspring/sevices/AdminService.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/sevices/AdminService.java
@@ -16,29 +16,5 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AdminService {
 
-    private final userRepo userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final EmailService emailService;
 
-    public AuthenticationResponse createManager(RegisterRequest request) {
-        // Génération d'un mot de passe aléatoire
-        String rawPassword = UUID.randomUUID().toString().substring(0, 8);
-
-        var manager = User.builder()
-                .firstname(request.getFirstname())
-                .lastname(request.getLastname())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(rawPassword))
-                .role(Role.MANAGER)
-                .build();
-
-        userRepository.save(manager);
-
-        // Envoi d'email avec le mot de passe en clair
-        emailService.sendCredentials(request.getEmail(), rawPassword);
-
-        return AuthenticationResponse.builder()
-                .build();
-
-    }
 }
