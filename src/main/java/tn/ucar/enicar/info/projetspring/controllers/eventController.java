79bff078d02ed92005dd1e942d106f3e2da45219
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-04-13 14:08:05 +0100

    feat: implement EventController with createEvent endpoint and security authorization

--- src/main/java/tn/ucar/enicar/info/projetspring/controllers/eventController.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/controllers/eventController.java
@@ -1,11 +1,21 @@
 package tn.ucar.enicar.info.projetspring.controllers;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import tn.ucar.enicar.info.projetspring.entities.event;
+import tn.ucar.enicar.info.projetspring.sevices.EventService;
 
 @RestController
-@AllArgsConstructor
-@RequestMapping("/event")
-public class eventController {
+@RequestMapping("/api/v1/event")
+@RequiredArgsConstructor
+public class EventController {
+    private final EventService eventService;
+
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<event> createEvent(@RequestBody event event) {
+        return ResponseEntity.ok(eventService.createEvent(event));
+    }
 }
