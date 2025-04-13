f35e44e9a8f1741b96658aca9e7bc988f35a4428
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-04-13 14:09:11 +0100

    feat: refactor UserService to implement service layer with Lombok annotations and role management

--- src/main/java/tn/ucar/enicar/info/projetspring/sevices/userService.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/sevices/userService.java
@@ -1,9 +1,19 @@
 package tn.ucar.enicar.info.projetspring.sevices;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import tn.ucar.enicar.info.projetspring.entities.Role;
 import tn.ucar.enicar.info.projetspring.entities.User;
+import tn.ucar.enicar.info.projetspring.entities.UserScoreDTO;
+import tn.ucar.enicar.info.projetspring.repositories.userRepo;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface userService {
-    public List<User> retrieveAllUsers();
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+
 }
